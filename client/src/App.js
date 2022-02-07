e6c96b3098d70d58b8c04adc9a884fd33d519b98
SDBoahen <SDBoahen@gmail.com>
2022-02-06 20:14:37 -0500

    Sam 2-6-2022 : Started setting-up a Controlled Form with a Controlled Input Field for our email input field - will translate that over next time to the other input fields  :)

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+import { useState } from 'react'
+
+
+
+
 function App() {
+
+  // const [ v , f ] = useState( initial/defaule STATE of v )
+  const [ emailInputFieldValue, updateEmailInputFieldValue ] = useState( "" )
+    console.log("STATE OF  emailInputFieldValue: ", emailInputFieldValue)  //
+    // -x- A NO-NO : updateEmailInputFieldValue("5-EVER")
+    ////  Calling a  useState  setter function outside of  our  return/render  
+    ////  AND/OR  NOT in a useEffect, will cause the setter function to keep being called, 
+    ////  and the Component to keep reading it, and rerendering it
+    
+    
 
 
 
 
   return (
     <div className="App">
+
       <header className="App-header">
 
         <img src={logo} className="App-logo" alt="logo" />
 
+
+
+
         <h1>- Nav Bar -</h1>
 
+        <br/><br/><br/><br/>
+
+
+
+
+
+
+
+
         <h1>- Login Form -</h1>
-          <form onSubmit={ 
+
+          <form onSubmit={
             
             ( sythEvent )=>{  sythEvent.preventDefault()
 
@@ -26,20 +56,60 @@ function App() {
           }>
             {/* .addEventListener( "submit" , (e)=>{} ) */}
 
-            <input />
+            <label >Email:</label>
+            <input onChange={
 
-            <input type="submit" />
-            <button type="submit" >S</button>
+              ( sythEvent )=>{
+                // console.log("onChange synthEVent: ", sythEvent)  //
+                // console.log("onChange synthEVent.target: ", sythEvent.target)  //
+                // console.log("onChange synthEVent.target.value: ", sythEvent.target.value)  //
+                // console.log("...typing")  //
+
+                updateEmailInputFieldValue( sythEvent.target.value )
+              }
+
+            }
+
+              value={emailInputFieldValue}
+            
+            /><br/>
+
+            <label >Username:</label>
+            <input /><br/>
+
+            <label >Password:</label>
+            <input /><br/>
+
+            <input value="Login!🚪💫✨" type="submit" />
+            {/* <button type="submit" >S</button> */}
+
 
           </form>
+
+        <br/><br/><br/><br/>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         <h1>- SignUp Form -</h1>
 
         <img src={logo} className="App-logo" alt="logo" />
 
       </header>
+
     </div>
   );
 }
-
 export default App;
